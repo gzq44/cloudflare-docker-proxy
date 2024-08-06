125aea943a66ccbcda69f146b1d1602134fa3a5f
gzq44 <72932294+gzq44@users.noreply.github.com>
2024-08-06 22:20:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.giz-channel.cloud": dockerHub,
+  "quay.giz-channel.cloud": "https://quay.io",
+  "gcr.giz-channel.cloud": "https://gcr.io",
+  "k8s-gcr.giz-channel.cloud": "https://k8s.gcr.io",
+  "k8s.giz-channel.cloud": "https://registry.k8s.io",
+  "ghcr.giz-channel.cloud": "https://ghcr.io",
+  "cloudsmith.giz-channel.cloud": "https://docker.cloudsmith.io",
+  "ecr.giz-channel.cloud": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.giz-channel.cloud": dockerHub,
 };
 
 function routeByHosts(host) {
